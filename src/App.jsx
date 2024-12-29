7658f5445c2b8d60f1833c3aa25161cd991b401d
amin <amindev2025@gmail.com>
2024-12-29 19:16:16 +0330

    fetching characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./App.css"
 import Navbar from './components/Navbar'
 import CharacterList from './components/CharacterList'
 import CharacterDetail from './components/CharacterDetail'
-import {allCharacters} from "../data/data"
+import { allCharacters } from "../data/data"
 
 
 function App() {
+  const [characters, setCharacters] = useState([])
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacters(data.results.slice(0, 5)))
+  }, [])
+
+
   return (
     <div className='app'>
+     
       <Navbar />
       <div className='main'>
-        <CharacterList characters={allCharacters} />
+        <CharacterList characters={characters} />
         <CharacterDetail />
       </div>
     </div>
